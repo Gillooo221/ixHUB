f6908c129e9a4f9142fde77a784ad6e11f5c619e
Ixroevich <155840318+Gillooo221@users.noreply.github.com>
2024-01-09 19:22:59 +0300

    Update PET99.lua

--- PET99.lua
+++ PET99.lua
@@ -1,27 +1,28 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+ 
 local Window = Rayfield:CreateWindow({
-   Name = "Pet Simulator 99 ixHUB",
-   LoadingTitle = "NIKITA SO COOL BOY....",
-   LoadingSubtitle = "by Ixroevich",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-   Discord = {
-      Enabled = true,
-      Invite = "E7cx9RtUXh", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Key System",
-      Subtitle = "Gleb is so fucked up that he made a key system",
-      Note = "The key can be obtained from the Discord server",
-      FileName = "KeyPet99", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://raw.githubusercontent.com/XroyYT2281/tolopt99/main/kelosk.lua"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+	Name = "Pet Simulator 99 ixHUB",
+	LoadingTitle = "NIKITOS SO COOL BOY...",
+	LoadingSubtitle = "by Ixroevich",
+	ConfigurationSaving = {
+		Enabled = true,
+		FolderName = nil, -- Create a custom folder for your hub/game
+		FileName = "ixHUB"
+	},
+        Discord = {
+        	Enabled = true,
+        	Invite = "E7cx9RtUXh", -- The Discord invite code, do not include discord.gg/
+        	RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+        },
+	KeySystem = true, -- Set this to true to use our key system
+	KeySettings = {
+		Title = "ixHUB",
+		Subtitle = "Key System",
+		Note = "Join the discord (https://discord.gg/E7cx9RtUXh)",
+		FileName = "ixHUB",
+		SaveKey = true,
+		GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+		Key = 'https://discord.gg/E7cx9RtUXh'
+	}
 })
