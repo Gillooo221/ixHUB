4e9b07acab1b7d935f828335624adf2196880035
Ixroevich <155840318+Gillooo221@users.noreply.github.com>
2024-01-10 16:58:04 +0300

    Update PET99.lua

--- PET99.lua
+++ PET99.lua
@@ -6,23 +6,23 @@ local Window = Rayfield:CreateWindow({
    LoadingSubtitle = "by Ixroevich",
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
+      FolderName = nil,
       FileName = "Big Hub"
    },
    Discord = {
       Enabled = true,
-      Invite = "E7cx9RtUXh", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Invite = "E7cx9RtUXh",
+      RememberJoins = true
    },
-   KeySystem = true, -- Set this to true to use our key system
+   KeySystem = true,
    KeySettings = {
       Title = "ixHUB ║ Key System",
       Subtitle = "Gleb is so fucked up that he made a key system",
       Note = "The key can be obtained from the Discord server: \nhttps://discord.gg/E7cx9RtUXh",
-      FileName = "KeyPet99", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = True, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Nikitos"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      FileName = "KeyPet99",
+      SaveKey = True,
+      GrabKeyFromSite = false,
+      Key = {"Nikitos"}
    }
 })
 
@@ -31,7 +31,7 @@ Rayfield:Notify({
    Content = "Pet Simulator 99 ║ ixHUB",
    Duration = 6.5,
    Image = 4483362458,
-   Actions = { -- Notification Buttons
+   Actions = {
       Ignore = {
          Name = "Okay!",
          Callback = function()
