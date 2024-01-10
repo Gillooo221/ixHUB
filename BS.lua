9043f12283f02422405c8901abd7e7eeaa1d3cf9
Ixroevich <155840318+Gillooo221@users.noreply.github.com>
2024-01-10 17:28:09 +0300

    Update BS.lua

--- BS.lua
+++ BS.lua
@@ -45,8 +45,8 @@ local Tab = Window:CreateTab("Teleport", 4483362458)
 local Section = Tab:CreateSection("Select Location")
 
 local Button = Tab:CreateButton({
-   Name = "Button Example",
+   Name = "Dandelion field",
    Callback = function()
-   -- The function that takes place when the button is pressed
+   game.Workspace.LocalPlayer.HumanoidRootPart.CFrame = -30.7356339, 4.76142693, 217.214432, -0.992111564, -9.48625267e-09, 0.125357896, -5.87130877e-09, 1, 2.92064506e-08, -0.125357896, 2.82400432e-08, -0.992111564
    end,
 })
