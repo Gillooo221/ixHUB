679645e08a8e868059fd502a0ccddedaaf311c43
Ixroevich <155840318+Gillooo221@users.noreply.github.com>
2024-03-12 22:30:42 +0300

    Update BSS.lua

--- BSS.lua
+++ BSS.lua
@@ -46,15 +46,9 @@ local Tab = Window:CreateTab("AutoFarm", 4483362458)
 local Section = Tab:CreateSection("Select Location")
 
 local Button = Tab:CreateButton({
-   Name = "Auto VendingMachine",
+   Name = "AutoFarm SunFlower",
    Callback = function() --- начало
-function sleep(seconds)
-    os.execute("sleep " .. seconds)
-end
-local args = {
-    [1] = "Cherry Blossom"
-}
-game:GetService("ReplicatedStorage").Network.Teleports_RequestTeleport:InvokeServer(unpack(args))
+function(7, -219.71196, 4.75390339, 158.496765, 0.987253964, -1.90416927e-09, 0.159152925, -1.04668638e-08, 1, 7.68922206e-08, -0.159152925, -7.75779796e-08, 0.987253964)
    end, --- конец
 })
 
