21ebbdc18571303cbce44a98ccddba09a8dd743b
Ixroevich <155840318+Gillooo221@users.noreply.github.com>
2024-03-12 17:22:59 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,6 +5,9 @@ local PlaceId = {
 if game.PlaceId == 8737899170 then
     GameLoad = "PET99"
 end
+if game.PlaceId == 15532962292 then
+    GameLoad = "sol"
+end
 local var,err = pcall(function ()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Gillooo221/ixHUB/main/" .. GameLoad .. ".lua"))()
 end)
