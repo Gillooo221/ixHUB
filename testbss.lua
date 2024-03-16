b0b70eaf203c82e4afaee1cc9759144a0282d4c7
Ixroevich <155840318+Gillooo221@users.noreply.github.com>
2024-03-16 11:00:19 +0300

    Delete testbss.lua

--- testbss.lua
+++
@@ -1,5 +0,0 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-getgenv().api = loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxking776/kocmoc/main/api.lua"))()
-getgenv().SelectedOption = nil
-
-local Window = OrionLib:MakeWindow({Name = "Bee Swarm Simulator ║ GptHub", HidePremium = false, SaveConfig = true, ConfigFolder = "GptHub"})
